159d81f91b74fd365f9875a7ec826ca3541b19b7
Ivan Tsar <ivan.tsar99@gmail.com>
2018-03-01 18:22:28 +0300

    7th solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let count5 = count2 = 0;
+    //let number = 8;
+    for (let i = 5; i <= number; i += 5) {
+        let temp = i;
+    
+        while (temp % 5 === 0) {
+            temp = temp / 5;
+            count5++;
+        }
+    } 
+    return count5;
+/*
+    for (let i = 2; i <= number; i += 2) {
+      let temp = i;
+  
+      while (temp % 2 === 0) {
+          temp = temp / 2;
+          count2++;
+      }
+  }
+    if (count2 >= count5){
+      return count5;
+    } else {
+      return count2;
+    }*/
 }
